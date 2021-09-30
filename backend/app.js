3e5d5220a94f3b808aaa64f39c4b52752d5d97fa
ooma tell <otymitu@yahoo.com>
2021-09-30 18:33:07 -0400

    Request,Response,Request received  HTTP - Apply successfuly

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,10 @@ const express = require('express');//create a constant named "express"
 const app = express();// create a constant "app" alows to create a application express
 
 app.use((req, res, next) => { 
-   console.log('Request Received');
+   console.log('Request Received !!!');
    next();
 });
+
 app.use((req, res, next) => { 
    res.status(555);//reponse status
    next();
@@ -14,7 +15,16 @@ app.use((req, res, next) => {
 
 
 app.use((req, res, next) => { //methode request / response with function 'next'
-   res.json({ message: 'YOUR Request Has Been  Received  !' }); //message return en json
+   res.json({ message: 'YOUR Request Has Been  Received  !!!' }); //message return en json HTTP page
+   next();
 });//lignes 5.6.7 is a function in app Express  and is a middleware
+
+
+app.use((req,res) => {
+   console.log('Response sent successfuly  !!! ');
+
+
+});
+
 
 module.exports = app;//export the app so that we can acces'it in the other files
