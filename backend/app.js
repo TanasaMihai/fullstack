5366aa5812d686000791d4fde81ffb89d5ae43bb
ooma tell <otymitu@yahoo.com>
2021-09-30 18:16:41 -0400

    Added network status 555 and console reponse

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,19 @@ const express = require('express');//create a constant named "express"
 
 const app = express();// create a constant "app" alows to create a application express
 
-app.use((req, res) => { //methode request / response with
-   res.json({ message: 'YOUR Request Has Been  Received  !' }); //message return en json
+app.use((req, res, next) => { 
+   console.log('Request Received');
+   next();
 });
+app.use((req, res, next) => { 
+   res.status(555);//reponse status
+   next();
+});
+
+
+
+app.use((req, res, next) => { //methode request / response with function 'next'
+   res.json({ message: 'YOUR Request Has Been  Received  !' }); //message return en json
+});//lignes 5.6.7 is a function in app Express  and is a middleware
 
 module.exports = app;//export the app so that we can acces'it in the other files
