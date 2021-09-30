ed3aa4bdc25554c07b92655404899c0f755d1e2c
ooma tell <otymitu@yahoo.com>
2021-09-30 01:02:19 -0400

    creation of 'app' file that will contain our application

---
+++ backend/app.js
@@ -0,0 +1,7 @@
+const express = require('express');//create a constant named "express"
+
+const app = express();// create a constant "app" alows to create a application express
+
+
+
+module.exports = app;//export the app so that we can acces'it in the other files
