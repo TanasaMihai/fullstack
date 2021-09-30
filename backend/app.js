7c452e99b0be8da8c86da347e40846908c6ab542
ooma tell <otymitu@yahoo.com>
2021-09-30 18:01:36 -0400

    test api successfuly-reponse from server OK

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,8 @@ const express = require('express');//create a constant named "express"
 
 const app = express();// create a constant "app" alows to create a application express
 
-app.use((req, res) => { //methode request response
-   res.json({ message: 'YOUR Request Has Been  Received  !' }); //message return
+app.use((req, res) => { //methode request / response with
+   res.json({ message: 'YOUR Request Has Been  Received  !' }); //message return en json
 });
 
 module.exports = app;//export the app so that we can acces'it in the other files
