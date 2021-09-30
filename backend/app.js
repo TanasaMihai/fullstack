fd96c0ed6414f83ab09fb2165b104f309381f18d
ooma tell <otymitu@yahoo.com>
2021-09-30 01:18:06 -0400

    changes added to app.js and server.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');//create a constant named "express"
 
 const app = express();// create a constant "app" alows to create a application express
 
-
+app.use((req, res) => { //methode request response
+   res.json({ message: 'YOUR Request Has Been  Received  !' }); //message return
+});
 
 module.exports = app;//export the app so that we can acces'it in the other files
