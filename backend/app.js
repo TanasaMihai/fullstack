00379f32f6150f54c0a5917d2bfb263487e55030
ooma tell <otymitu@yahoo.com>
2021-09-30 19:15:26 -0400

    dd

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require('express');                                // create a constant "app" alows to create a application express
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+console.log('Request Received !!!');
   next();
 });
 
 app.use((req, res, next) => {
-  res.status(201);
+  res.status(555);                                                 //reponse status
   next();
 });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+app.use((req, res, next) => {                                      //methode request / response with function 'next'
+    res.json({ message: 'YOUR Request Has Been  Received  !!!' }); //message return en json HTTP page
+  next();                                                          //lignes 5.6.7 is a function in app Express  and is a middleware
+});                                                                
 
 app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+    console.log('Response sent successfuly  !!! ');
 });
 
 module.exports = app;
