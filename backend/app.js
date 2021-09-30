419f65dc5bf615693a5abe95d85c2e650eee285c
ooma tell <otymitu@yahoo.com>
2021-09-30 19:09:41 -0400

    try to repair error with rm .git/index.lock

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,24 @@
-const express = require('express');//create a constant named "express"
+const express = require('express');
 
-const app = express();// create a constant "app" alows to create a application express
+const app = express();
 
-app.use((req, res, next) => { 
-   console.log('Request Received !!!');
-   next();
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
 });
 
-app.use((req, res, next) => { 
-   res.status(555);//reponse status
-   next();
+app.use((req, res, next) => {
+  res.status(201);
+  next();
 });
 
-
-
-app.use((req, res, next) => { //methode request / response with function 'next'
-   res.json({ message: 'YOUR Request Has Been  Received  !!!' }); //message return en json HTTP page
-   next();
-});//lignes 5.6.7 is a function in app Express  and is a middleware
-
-
-app.use((req,res) => {
-   console.log('Response sent successfuly  !!! ');
-
-
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
 });
 
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
 
-module.exports = app;//export the app so that we can acces'it in the other files
+module.exports = app;
